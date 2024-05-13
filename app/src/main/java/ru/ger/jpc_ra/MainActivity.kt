f1d559246ad70a5847a832a27a22f43e108722c5
Gerasim <g-926@ya.ru>
2024-05-13 22:29:56 +0300

    6.1

--- app/src/main/java/ru/ger/jpc_ra/MainActivity.kt
+++ app/src/main/java/ru/ger/jpc_ra/MainActivity.kt
@@ -7,17 +7,20 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.BiasAbsoluteAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    /**AndroidComposableView*/
         setContent {
         HelloWorld()
         }
@@ -32,8 +35,9 @@ fun HelloWorld(){
        // verticalAlignment = Alignment.CenterVertically,
         horizontalAlignment = BiasAbsoluteAlignment.Horizontal(-0.3f),
         verticalArrangement = Arrangement.SpaceAround,
-        modifier = Modifier.background(Color.Cyan)
-        .fillMaxSize()
+        modifier = Modifier
+            .background(Color.Cyan)
+            .fillMaxSize()
     ) {
         Text(
             "Hello World",
@@ -46,7 +50,10 @@ fun HelloWorld(){
             "Racoon",
             fontSize = 20.sp,
             color = Color.Blue,
-            modifier = Modifier.background(Color.Green)
+            modifier = Modifier
+                .background(Color.Blue)
+                .padding(10.dp)
+                .background(Color.Green)
                 .align(BiasAbsoluteAlignment.Horizontal(0.6f))
                 .weight(2f)
         )
