3d2748facb2f89dfb555dda22ea48c8808c39e64
Gerasim <g-926@ya.ru>
2024-05-13 22:04:46 +0300

    5.2

--- app/src/main/java/ru/ger/jpc_ra/MainActivity.kt
+++ app/src/main/java/ru/ger/jpc_ra/MainActivity.kt
@@ -5,16 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.BiasAbsoluteAlignment
 import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 
 class MainActivity : ComponentActivity() {
@@ -30,10 +33,10 @@ class MainActivity : ComponentActivity() {
 )
 @Composable
 fun HelloWorld(){
-    Row(
+    Column(
        // verticalAlignment = Alignment.CenterVertically,
-        verticalAlignment = BiasAlignment.Vertical(-0.3f),
-        horizontalArrangement = Arrangement.SpaceAround,
+        horizontalAlignment = BiasAbsoluteAlignment.Horizontal(-0.3f),
+        verticalArrangement = Arrangement.SpaceAround,
         modifier = Modifier.background(Color.Cyan)
         .fillMaxSize()
     ) {
@@ -48,6 +51,13 @@ fun HelloWorld(){
             fontSize = 20.sp,
             color = Color.Blue,
             modifier = Modifier.background(Color.Green)
+                .align(BiasAbsoluteAlignment.Horizontal(0.6f))
+        )
+        Text(
+            "Peace",
+            fontSize = 45.sp,
+            color = Color.Green,
+            modifier = Modifier.background(Color.LightGray)
         )
     }
 }
