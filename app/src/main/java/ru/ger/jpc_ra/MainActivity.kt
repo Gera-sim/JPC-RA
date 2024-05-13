56b3e35c6689be4d2b17b002b7a13c5c4ae8d370
Gerasim <g-926@ya.ru>
2024-05-13 21:55:53 +0300

    5.1

--- app/src/main/java/ru/ger/jpc_ra/MainActivity.kt
+++ app/src/main/java/ru/ger/jpc_ra/MainActivity.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,13 +25,29 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-@Preview(showBackground = true)
+@Preview(
+    showSystemUi = true
+)
 @Composable
 fun HelloWorld(){
-    Text(
-        "Hello World",
-        fontSize = 35.sp,
-        color = Color.Red,
-        modifier = Modifier.background(Color.Yellow)
-    )
+    Row(
+       // verticalAlignment = Alignment.CenterVertically,
+        verticalAlignment = BiasAlignment.Vertical(-0.3f),
+        horizontalArrangement = Arrangement.SpaceAround,
+        modifier = Modifier.background(Color.Cyan)
+        .fillMaxSize()
+    ) {
+        Text(
+            "Hello World",
+            fontSize = 35.sp,
+            color = Color.Red,
+            modifier = Modifier.background(Color.Yellow)
+        )
+        Text(
+            "Racoon",
+            fontSize = 20.sp,
+            color = Color.Blue,
+            modifier = Modifier.background(Color.Green)
+        )
+    }
 }
